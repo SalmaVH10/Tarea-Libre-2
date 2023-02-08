9025bbf92e62a7313328953e315b3cf2b41699d9
SalmaVH10 <123529535+SalmaVH10@users.noreply.github.com>
2023-02-07 23:35:03 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+let numero = 10;
+numero **= 5;
+
+document.write(numero)
+
+/*const contenedor = document.getElementById("contenedor")
+let btn1 = document.getElementById("btn")
+console.log(contenedor);
+
+function saludos(){
+    for(var i=0; i<10; i++){
+        contenedor.innerHTML += "<h2>Hola Mundo</h2>"
+    }
+
+}
+
+btn1.addEventListener("click", saludos);*/
+
+let nombre = prompt("dime tu nombre")
+alert("hola " + nombre)
+
+//let numero = null;
+//alert(numero)
+
+//let numero = 5;
+//let numero2 = 7;
+//alert(numero * numero2)
+
+//let numero = 15;
+
+//string = "cadena de texto"
+//number = 19
+//boolenao = false
+
+//recipiente = "pedro";
+
+//alert(recipiente)
